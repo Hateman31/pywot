096edfbfb47d4fe0a2c7824d305f2f35c47e023e
Hateman31 <metilogrunge31@gmail.com>
2015-04-05 13:38:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,7 @@ setup(
 	packages=['pywot'], 
 	version='0.1', 
 	description='A python interface to Wargaming.net\'s World of Tanks API', 
-	install_requires=[
-		'requests'
-	],
+	install_requires=['requests'],
 	author='Matt Selph', 
 	author_email='matt.selph@gmail.com', 
 	url='https://github.com/mattselph/pywot', 
